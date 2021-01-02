2191b6ac7230eb4ae1e9ea9dc1f36aa6de7b9f41
afonsocaiado <afonsocaiado1@gmail.com>
2021-01-02 18:11:17 +0000

    progress

--- tp2/padeiro_da_vila.pl
+++ tp2/padeiro_da_vila.pl
@@ -5,6 +5,11 @@ solve:-
     padeiro_da_vila([85, 98, 60, 70], [[0, 3, 14, 1], [15, 0, 10, 25], [14, 10, 0, 3], [1, 25, 3, 0]], [10, 5, 20, 15])
 .
 
-padeiro_da_vila(TempoPreferidoCadaCasa, TempoEntreCasas, TempoPadariaCasas):-
-
+padeiro_da_vila(TempoPreferidoCadaCasa,TempoEntreCasas,TempoPadariaCasas):-
+    length(TempoPreferidoCadaCasa,NumeroCasas),
+    length(Rota, NumeroCasas),
+    length(MomentoEntrega,NumeroCasas),
+    
+    domain(Rota,1,NumeroCasas),
+    domain(MomentoEntrega,1,100000),
 .
