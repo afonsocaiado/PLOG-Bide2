09c02493b1403880b5bcd0e81904de78df885eb8
afonsocaiado <afonsocaiado1@gmail.com>
2021-01-02 18:04:05 +0000

    Beginning of padeiro da vila

---
+++ tp2/padeiro_da_vila.pl
@@ -0,0 +1,10 @@
+:-use_module(library(clpfd)).
+:-use_module(library(lists)).
+
+solve:-
+    padeiro_da_vila([85, 98, 60, 70], [[0, 3, 14, 1], [15, 0, 10, 25], [14, 10, 0, 3], [1, 25, 3, 0]], [10, 5, 20, 15])
+.
+
+padeiro_da_vila(TempoPreferidoCadaCasa, TempoEntreCasas, TempoPadariaCasas):-
+
+.
